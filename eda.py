addb592a5a32152a7bc111c7db164e4597c7112f
Luke Woloszyn <luke.woloszyn@gmail.com>
2016-07-26 18:01:46 -0700

    continue regex madness

--- eda.py
+++ eda.py
@@ -17,8 +17,11 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.decomposition import TruncatedSVD
 from sklearn.decomposition import LatentDirichletAllocation
 
+from stop_words import get_stop_words
+# import requests
 import nltk.corpus
 import re
+import string
 
 
 def get_documents(collection='pitchfork_full'):
@@ -147,21 +150,66 @@ def basic_lsi(df):
     return tfidf, svd
 
 
-def print_components_and_albums(tfidf, svd, df, n_words=10, n_docs=10):
-    transformed = svd.transform(tfidf.transform(df['review']))
-    top_docs = np.argsort(transformed, axis=0)
+def extended_lsi(df):
+    abstracts = df['abstract']
+    reviews = df['review']
+    genres = df['genres'].map(lambda x: ' '.join(x))
+    artists = df['artists'].map(lambda x: ' '.join(x))
+    album = df['album']
+
+    together = [abstracts, reviews, genres, artists, album]
+
+    entries = [' '.join(entry) for entry in zip(*together)]
+
+    # r = requests.get('http://fs1.position2.com/bm/txt/stopwords.txt')
+    # stopwords = r.content.split('\n')
+    # stopwords = nltk.corpus.stopwords.words('english')
+    stopwords = get_stop_words('en')
+    # with open('data/stopwords.txt') as f:
+    #    stopwords = f.readlines()
+    #    stopwords = [stopword.strip() for stopword in stopwords]
+
+    # get those contractions
+    stopwords.extend(nltk.word_tokenize(' '.join(stopwords)))
+
+    # custom stop words
+    stopwords.extend(['lp', 'ep',
+                      'record', 'records',
+                      'feel', 'feels',
+                      'sound', 'sounds',
+                      'album', 'albums',
+                      'song', 'songs',
+                      'music'])
+    stopset = set(stopwords)
+
+    tfidf = TfidfVectorizer(stop_words=stopset,
+                            preprocessor=CustomTextPreprocessor(),
+                            tokenizer=CustomTokenizer(stopset),
+                            max_df=0.5, min_df=5)
+    tfidf_trans = tfidf.fit_transform(entries)
+
+    svd = TruncatedSVD(n_components=100)
+    svd_trans = svd.fit_transform(tfidf_trans)
+
+    return tfidf, tfidf_trans, svd, svd_trans
+
+
+def print_components(tfidf, svd, n_words=10, n_docs=10):
+    # transformed = svd.transform(tfidf.transform(df['review']))
+    # top_docs = np.argsort(transformed, axis=0)
 
     components = svd.components_
     words = np.array(tfidf.get_feature_names())
     for i, component in enumerate(components[0:10], 1):
         sorted_idx = np.argsort(component)
         print('Component #{:d}'.format(i))
-        print('Most negative words:')
-        print(' '.join(words[sorted_idx[:n_words]]))
-        print('Most positive words:')
-        print(' '.join(words[sorted_idx[-n_words:]]))
-        print('Top albums:')
-        print(df['url'].iloc[top_docs[-n_docs:, i-1]])
+        print('-'*20)
+        print('\nMost negative words:')
+        print('\n\t'+'\n\t'.join(words[sorted_idx[:n_words]]))
+        print('\nMost positive words:')
+        print('\n\t'+'\n\t'.join(words[sorted_idx[-n_words:]]))
+        # print('Top albums:')
+        # print('\t\n'.join(df['url'].iloc[top_docs[-n_docs:, i-1]]))
         print('\n')
 
 
@@ -178,23 +226,6 @@ def basic_lda(df):
 
     return lda_trans
 
-
-
-
-#  pY=repmat(pY,1,size(p_X_giv_Y,2));
-
-#  %marginal probability of x
-#  p_X=sum(p_X_and_Y,1);
-
-#  %inner sum of conditional entropy
-#  cond_ent_tmp=-p_X_giv_Y.*log2(p_X_giv_Y)-(1-p_X_giv_Y).*log2(1-p_X_giv_Y);
-#  %outer sum of conditional entropy
-#  cond_ent=sum(cond_ent_tmp.*pY,1);
-#  marg_ent=-p_X.*log2(p_X)-(1-p_X).*log2(1-p_X);
-#  ent=marg_ent-cond_ent;
-
-#  [vals idx]=sort(ent,'descend');
-#  idx=idx(1:numfeats);
 
 class PorterTokenizer(object):
 
@@ -223,22 +254,128 @@ def print_top_words(vectorizer, clf, class_labels, n=10):
 #        print('Component {:d}: {:s}'.format(i,
 #            ' '.join([words[j] for j in top10])))
 
+class CustomTokenizer(object):
+    def __init__(self, stopset=None, stemmer=PorterStemmer()):
+        self.tokenizer = nltk.tokenize.word_tokenize
+        self.punctset = set(string.punctuation)
+        if stopset:
+            self.stopset = set(stopset)
+        if stemmer:
+            self.stemmer = PorterStemmer()
 
-def unicode_preprocessing(text):
-    single_quotes = ur"""['\u2018\u2019\u0060\u00b4]"""
-    text = re.sub(single_quotes, "'", text)
+    def __call__(self, doc):
+        # use nltk's intelligent tokenizer, but we'll do some postprocessing
+        words = self.tokenizer(doc)
 
-    double_quotes = ur"""["\u201c\u201d]"""
-    text = re.sub(double_quotes, '"', text)
+        # now split on hyphens manually
+        words = [subword for word in words for subword in word.split('-')]
 
-    # pound_signs = ur"""#"""
-    # text = re.sub(pound_signs, '-', text)
+        # remove stopwords/punctuation
+        words = filter(lambda x: (x not in self.punctset and
+                                  x not in self.stopset and
+                                  x.strip(string.punctuation)),
+                       words)
 
-    space = ur"""\xa0"""
-    text = re.sub(space, ' ', text)
+        # strip punctuation and stem
+        words = [self.stemmer.stem(word.strip(string.punctuation))
+                 for word in words]
 
-    em_dashes = ur"""\u2014"""
-    text = re.sub(em_dashes, '--', text)
+        return words
 
-    text = re.sub(ur'/', ' / ', text)
-    return text
+
+class CustomTextPreprocessor(object):
+    def __init__(self):
+        self.u_single_quotes = ur"['\u2018\u2019\u0060\u00b4]"
+        self.u_double_quotes = ur'["\u201c\u201d]'
+        self.u_spaces = ur'\xa0'
+        self.u_en_dashes = ur'\u2013'
+        self.u_em_dashes = ur'\u2014'
+        self.u_infinity_signs = ur'\u221e'
+
+    def _lowercase(self, text):
+        return text.lower()
+
+    def _preprocess_unicode(self, text):
+        text = re.sub(self.u_single_quotes, "'", text)
+        text = re.sub(self.u_double_quotes, '"', text)
+        text = re.sub(self.u_spaces, ' ', text)
+        text = re.sub(self.u_en_dashes, '-', text)
+        text = re.sub(self.u_em_dashes, '--', text)
+        text = re.sub(self.u_infinity_signs, '__INF__', text)
+        return text
+
+    def _preprocess_custom_general(self, text):
+        # doing this so nltk tokenizer works better
+        text = re.sub(r'\$', '__DOLLAR_SIGN__', text)
+        text = re.sub(r'%', '__PERCENT_SIGN__', text)
+        text = re.sub(r'\^', '__CARET__', text)
+        text = re.sub(r'&', '__AMPERSAND__', text)
+        text = re.sub(r'\*', '__ASTERISK__', text)
+
+        # deal with music "decades": change e.g. 1980 to '80s and '80s to 80s
+        text = re.sub(r"(19|20)(\d0)s", r"'\2s", text)
+        text = re.sub(r"'(\d0s)", r"\1", text)
+
+        # get rid of pesky newlines, carriages, tabs which screw up tokenizer
+        text = re.sub(r'(\n|\r|\t)', ' ', text)
+
+        return text
+
+    def _preprocess_custom_specific(self, text):
+        # band chk chk chk
+        text = re.sub(r"""\B!!!([\s,.'"])""", r'chk chk chk\1', text)
+
+        # bands like Matt & Kim
+        text = re.sub(r'\b([A-Z][a-z]*)(\s+)&(\s+)+([A-Z][a-z]*)\b',
+                      r'\1__AMPERSAND__\4', text)
+
+        ################################
+        #  custom genre manipulation   #
+        ################################
+
+        # genres that have a -rock, -pop, etc.
+        text = re.sub(r'(\w+)-rock\b', r'\1_rock', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-pop\b', r'\1_pop', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-punk\b', r'\1_punk', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-metal\b', r'\1_metal', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-country\b', r'\1_country', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-blues\b', r'\1_blues', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-hop', r'\1_hop', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-rap\b', r'\1_rap', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-R&B\b', r'\1_R&B', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-jazz\b', r'\1_jazz', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-soul\b', r'\1_soul', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-classical\b', r'\1_classical', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-contemporary\b', r'\1_contemporary', text,
+                      flags=re.IGNORECASE)
+        text = re.sub(r'(\w+)-techno\b', r'\1_techno', text,
+                      re.IGNORECASE)
+        text = re.sub(r'(\w+)-electronica\b', r'\1_electronica', text,
+                      flags=re.IGNORECASE)
+
+        # split quoted verses (usually rap lyrics)
+        text = re.sub(r'/', ' / ', text)
+
+        return text
+
+    def __call__(self, text):
+        text = self._preprocess_unicode(text)
+        text = self._preprocess_custom_specific(text)
+        return text
+        text = self._preprocess_custom_general(text)
+
+        # lowercase last!
+        text = self._lowercase(text)
+        return text
