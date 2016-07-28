cb7a5c47f1a98f2b4ac1d6ec215ee9304922d96b
Luke Woloszyn <luke.woloszyn@gmail.com>
2016-07-27 22:07:03 -0700

    sql query to compute mean spotify features

---
+++ src/audio_features.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import psycopg2
+import pandas as pd
+
+
+def get_mean_audio_features():
+    conn = psycopg2.connect(database='albumpitch', user='lukewoloszyn')
+    cur = conn.cursor()
+
+    try:
+        cur.execute("""
+        SELECT p.id, avg(acousticness), avg(danceability),
+        avg(energy), avg(instrumentalness), avg(key),
+        avg(liveness), avg(loudness), avg(speechiness),
+        avg(tempo), avg(time_signature), avg(valence)
+        FROM pitchfork p
+        JOIN spotify_albums sa ON p.spotify_id = sa.id
+        JOIN spotify_audio_features saf ON sa.id = saf.album_id
+        GROUP BY p.id;
+        """)
+        result = cur.fetchall()
+    finally:
+        conn.close()
+
+    df = pd.DataFrame(result, columns=['review_id', 'acoustic', 'dance',
+                                       'energy', 'instrument', 'key',
+                                       'live', 'loud', 'speech', 'tempo',
+                                       'time_signature', 'valence'])
+    return df
