bb50b94dc6f674eefd1f417b38905b61bef0188a
Luke Woloszyn <luke.woloszyn@gmail.com>
2016-07-27 21:40:41 -0700

    transfer data from mongo to sql for easier queries

---
+++ src/mongo_to_sql.py
@@ -0,0 +1,177 @@
+from pymongo import MongoClient
+import psycopg2
+
+
+def insert_pitchfork_reviews():
+    conn = psycopg2.connect(database='albumpitch', user='lukewoloszyn')
+    cur = conn.cursor()
+
+    client = MongoClient()
+    db = client['albumpitch']
+    coll = db['pitchfork']
+
+    try:
+        for doc in coll.find():
+            pid = doc['review_id']
+            url = doc['url']
+
+            spotify_id = doc.get('putative_spotify_id', None)
+
+            artist = ' / '.join(doc['artists'])
+            album = doc['album']
+            genres = ', '.join(doc.get('genres', [None]))
+
+            pub_date = doc['pub_date']
+            reviewers = ', '.join(doc['reviewers'])
+
+            labels = ', '.join(doc['labels'])
+            year = doc.get('year', None)
+
+            score = doc['score']
+            abstract = doc['abstract']
+            review = doc['review']
+
+            SQL = """
+            INSERT INTO pitchfork (id, url, spotify_id, artist, album, genres,
+            pub_date, reviewers, labels, year, score, abstract, review)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT DO NOTHING;
+            """
+            data = (pid, url, spotify_id, artist, album, genres,
+                    pub_date, reviewers, labels, year, score, abstract, review)
+            cur.execute(SQL, data)
+    finally:
+        conn.commit()
+        conn.close()
+        client.close()
+
+
+def insert_audio_features():
+    conn = psycopg2.connect(database='albumpitch', user='lukewoloszyn')
+    cur = conn.cursor()
+
+    client = MongoClient()
+    db = client['albumpitch']
+    coll = db['spotify_audio_features']
+
+    try:
+        for doc in coll.find():
+            if 'id' not in doc:
+                print('Track with no id, skipping')
+                continue
+            tid = doc['id']
+            album_id = doc['album_id']
+            acoustic = doc['acousticness']
+            dance = doc['danceability']
+            energy = doc['energy']
+            instrument = doc['instrumentalness']
+            key = doc['key']
+            liveness = doc['liveness']
+            loudness = doc['loudness']
+            mode = doc['mode']
+            speechiness = doc['speechiness']
+            tempo = doc['tempo']
+            ts = doc['time_signature']
+            valence = doc['valence']
+            SQL = """
+            INSERT INTO spotify_audio_features (id, album_id, acousticness,
+            danceability, energy, instrumentalness, key, liveness, loudness,
+            mode, speechiness, tempo, time_signature, valence)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT DO NOTHING;
+            """
+            data = (tid, album_id, acoustic, dance, energy, instrument, key,
+                    liveness, loudness, mode, speechiness, tempo, ts, valence)
+            cur.execute(SQL, data)
+    finally:
+        conn.commit()
+        conn.close()
+        client.close()
+
+
+def insert_spotify_album():
+    conn = psycopg2.connect(database='albumpitch', user='lukewoloszyn')
+    cur = conn.cursor()
+
+    client = MongoClient()
+    db = client['albumpitch']
+    coll = db['spotify_albums']
+
+    try:
+        for doc in coll.find():
+            album_id = doc['id']
+            album_name = doc['name']
+            SQL = """
+            INSERT INTO spotify_albums (id, name)
+            VALUES (%s, %s)
+            ON CONFLICT DO NOTHING;
+            """
+            data = (album_id, album_name)
+            cur.execute(SQL, data)
+    finally:
+        conn.commit()
+        conn.close()
+        client.close()
+
+
+def create_table_pitchfork():
+    conn = psycopg2.connect(database='albumpitch', user='lukewoloszyn')
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS pitchfork (
+        id varchar(255) NOT NULL primary key,
+        url varchar(255) NOT NULL,
+        spotify_id varchar(255) default NULL REFERENCES spotify_albums (id),
+
+        artist varchar(255) NOT NULL,
+        album varchar(255) NOT NULL,
+        genres varchar(255) default NULL,
+
+        pub_date timestamp default NULL,
+        reviewers varchar(255) default NULL,
+
+        labels varchar(255) default NULL,
+        year varchar(255) default NULL,
+
+        score int NOT NULL,
+        abstract text default NULL,
+        review text NOT NULL);
+    """)
+    conn.commit()
+    conn.close()
+
+
+def create_table_audio_features():
+    conn = psycopg2.connect(database='albumpitch', user='lukewoloszyn')
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS spotify_audio_features (
+        id varchar(255) NOT NULL primary key,
+        album_id varchar(255) NOT NULL REFERENCES spotify_albums (id),
+        acousticness real default NULL,
+        danceability real default NULL,
+        energy real default NULL,
+        instrumentalness real default NULL,
+        key int default NULL,
+        liveness real default NULL,
+        loudness real default NULL,
+        mode int default NULL ,
+        speechiness real default NULL,
+        tempo real default NULL,
+        time_signature int default NULL,
+        valence real default NULL);
+    """)
+    conn.commit()
+    conn.close()
+
+
+def create_table_albums():
+    conn = psycopg2.connect(database='albumpitch', user='lukewoloszyn')
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS spotify_albums (
+        id varchar(255) NOT NULL primary key,
+        name varchar(255) NOT NULL);
+    """)
+    conn.commit()
+    conn.close()
