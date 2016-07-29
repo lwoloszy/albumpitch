e616d4b4e62cc35f307e9518bb59a4de49f768ef
Luke Woloszyn <luke.woloszyn@gmail.com>
2016-07-29 13:51:51 -0700

    add validation pipeline

---
+++ src/validate.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_distances, cosine_similarity
+from sklearn.preprocessing import StandardScaler
+
+sns.set_style('ticks')
+
+
+def get_similarities(df, svd_trans, func='raw', normalize=False, n=500):
+    # for validation, use only tracks that have audio information
+    # as scraped from spotify (and matched in pitchfork)
+    df['has_audio_features'] = ~df['acoustic'].isnull()
+
+    feature_names = ['acoustic', 'dance', 'energy', 'instrument', 'key',
+                     'live', 'loud', 'speech', 'tempo', 'valence']
+    features_and_sims = feature_names + ['sims']
+
+    valid_idx = np.where(df['has_audio_features'])[0]
+    df = df.iloc[valid_idx]
+    svd_trans = svd_trans[valid_idx]
+    sims = cosine_similarity(svd_trans, svd_trans)
+
+    if normalize:
+        ss = StandardScaler()
+        ss.fit(np.float64(df[feature_names].values))
+        df[feature_names] = ss.transform(df[feature_names])
+
+    features = np.float64(df[feature_names].values)
+    all_audio_diffs = []
+    all_lsi_sims = []
+
+    for i, (sim_vector, seed_features) in enumerate(zip(sims, features), 1):
+        if i % 100 == 0:
+            print('Gone through {:d} albums'.format(i))
+
+        df['sims'] = sim_vector
+        df_audiofeats = df[features_and_sims].sort_values('sims').iloc[-n:]
+        other_features = np.float64(df_audiofeats[feature_names].values)
+
+        if not hasattr(func, '__call__'):
+            feature_diffs = np.sqrt((other_features - seed_features)**2)
+            all_audio_diffs.append(
+                pd.DataFrame(feature_diffs[::-1],
+                             columns=feature_names).reset_index(drop=True))
+        else:
+            all_audio_diffs.append(
+                pd.DataFrame(
+                    func(seed_features.reshape(1, -1), other_features).T[::-1],
+                    columns=['sim_func']).
+                reset_index(drop=True))
+
+        all_lsi_sims.append(df_audiofeats['sims'][::-1].reset_index(drop=True))
+
+    return all_lsi_sims, all_audio_diffs
+
+
+def plot_af_diffs(all_audio_diffs):
+    fig = plt.figure()
+    feature_names = ['acoustic', 'dance', 'energy', 'instrument',
+                     'live', 'loud', 'speech', 'tempo', 'valence']
+    feature_names = ['sim_func']
+    colors = sns.color_palette('Set1', 10)
+    for i, feature_name in enumerate(feature_names):
+        temp = [seed[feature_name] for seed in all_audio_diffs]
+        df = pd.concat(temp)
+        df.index.name = 'recc_rank'
+        mean_diff = df.groupby(df.index).mean()[1:]
+        std_diff = df.groupby(df.index).std()
+        ax = fig.add_subplot(3, 3, i+1)
+        plot_indiv_diff(ax, mean_diff, std_diff, colors[i])
+
+    sns.despine(offset=5, trim=True)
+
+
+def plot_cos_sims(all_cos_sims):
+    plt.close('all')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    feature_name = ['sim_func']
+    df = pd.concat([seed[feature_name] for seed in all_cos_sims])
+    df.index.name = 'recc_rank'
+
+    mean_diff = df.groupby(df.index).mean()[1:].values.flatten()
+    sem_diff = df.groupby(df.index).sem()[1:].values.flatten()
+    # return df.groupby(df.index).std()
+
+    plot_indiv_diff(ax, mean_diff, sem_diff, 'cosine similarity', 'k')
+
+    inset_ax = fig.add_axes([.75, .5, .1, .3])
+    n = 500
+    lh, rh = mean_diff[:n/2], mean_diff[n/2:n]
+    m_lh, m_rh = np.mean(lh), np.mean(rh)
+    sem_lh, sem_rh = np.std(lh)/np.sqrt(n/2), np.std(rh)/np.sqrt(n/2)
+
+    N = np.arange(2)
+    width = 0.75
+    inset_ax.bar(N, [m_lh, m_rh], width=width, yerr=[sem_lh, sem_rh])
+    inset_ax.set_xticks(N + width/2.)
+    inset_ax.set_ylabel('cosine similarity')
+
+    sns.despine(offset=5, trim=True)
+
+    # gotta do this after despine
+    inset_ax.set_xticklabels(('Left half', 'Right half'),
+                             horizontalalignment='right', rotation=45)
+
+
+def plot_indiv_diff(ax, y, y_e, y_label, color):
+    x = np.arange(len(y))
+    ax.add_line(plt.Line2D(x, y, color=color))
+    ax.fill_between(x, y-y_e, y+y_e, color=color, alpha=0.5)
+    ax.set_xlabel('Recommendation rank')
+    ax.set_ylabel(y_label)
+    ax.set_xlim(0, 500)
+    ax.set_ylim(np.min(y), np.max(y)*1.1)
