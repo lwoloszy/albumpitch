12b86b2945dc02dab0536fcee21dc0768fef8a96
Luke Woloszyn <luke.woloszyn@gmail.com>
2016-08-08 15:45:33 -0700

    trying hellinger distance for lda

--- src/gensim_models.py
+++ src/gensim_models.py
@@ -1,6 +1,10 @@
+# playground for gensim stuff
+
 import logging
-from gensim import corpora, models, similarities
+import numpy as np
+from gensim import corpora, models, similarities, matutils
 import text_preprocess as textpre
+from sklearn.neighbors import DistanceMetric
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.INFO)
@@ -63,12 +67,18 @@ def create_lda(dictionary, corpus, num_topics=100, passes=1):
     return lda, corpus_lda
 
 
+def get_doc_topics(lda, bow):
+    gamma, _ = lda.inference([bow])
+    topic_dist = gamma[0] / sum(gamma[0])  # normalize distribution
+    return [(topicid, topicvalue) for topicid, topicvalue in enumerate(topic_dist)]
+
+
 def get_indexed_sims(model, corpus):
     index = similarities.MatrixSimilarity(model[corpus])
     return index
 
 
-def get_most_similar(df, seed_num, dictionary, tfidf, lsi, index, n=10):
+def print_most_similar_lsi(df, seed_num, dictionary, tfidf, lsi, index, n=10):
     vec_bow = dictionary.doc2bow(df['tokenized_text'].iloc[seed_num])
     vec_tfidf = tfidf[vec_bow]
     vec_lsi = lsi[vec_tfidf]
@@ -82,7 +92,7 @@ def get_most_similar(df, seed_num, dictionary, tfidf, lsi, index, n=10):
     print df_temp[['url', 'genres', 'sim_scores']]
 
 
-def get_most_similar2(df, seed_num, dictionary, lda, index, n=10):
+def print_most_similar_lda(df, seed_num, dictionary, lda, index, n=10):
     vec_bow = dictionary.doc2bow(df['tokenized_text'].iloc[seed_num])
     vec_lda = lda[vec_bow]
     sims = index[vec_lda]
@@ -93,3 +103,33 @@ def get_most_similar2(df, seed_num, dictionary, lda, index, n=10):
     df_temp = df.iloc[idxs]
     df_temp['sim_scores'] = scores
     print df_temp[['url', 'genres', 'sim_scores']]
+
+
+def print_most_similar_lda_hellinger(df, seed_num, X_dense, n=10):
+
+    def hellinger(v1, v2):
+        sim = np.sqrt(0.5 * ((np.sqrt(v1) - np.sqrt(v2))**2).sum())
+        return sim
+
+    dist = DistanceMetric.get_metric(hellinger)
+    dists = dist.pairwise(X_dense[seed_num, :].reshape(1, -1), X_dense).flatten()
+    idx = np.argsort(dists)
+    sims = 1 - dists
+    sims = sims[idx]
+
+    df_temp = df.iloc[idx[0:n]]
+    df_temp['sim_scores'] = sims[0:n]
+    print df_temp[['url', 'genres', 'sim_scores']]
+
+
+def lda_to_dense(lda, corpus):
+    X_lda = np.zeros((len(corpus), lda.num_topics))
+    for i, doc in enumerate(corpus):
+        X_lda[i, :] = matutils.sparse2full(get_doc_topics(lda, doc),
+                                           lda.num_topics)
+    return X_lda
+
+
+def hellinger(v1, v2):
+    sim = np.sqrt(0.5 * ((np.sqrt(v1) - np.sqrt(v2))**2).sum())
+    return sim
